4e1561cf4c5612423e4c6520968bcedcd1b1ed70
Flavio Silva <flv.alves@gmail.com>
2020-01-11 18:53:05 -0300

    basic validation

--- tests/Feature/ContactsTest.php
+++ tests/Feature/ContactsTest.php
@@ -17,9 +17,49 @@ class ContactsTest extends TestCase
         $this->withoutExceptionHandling();
 
         $this->post('api/contacts', [
-            'name' => 'Test name'
+            'name' => 'Test Name',
+            'email' => 'test@email.com',
+            'birthday' => '05/14/1988',
+            'company' => 'ABC String'
         ]);
 
-        $this->assertCount(1, Contact::all());
+        $contact = Contact::first();
+
+        $this->assertEquals('Test Name', $contact->name);
+        $this->assertEquals('test@email.com', $contact->email);
+        $this->assertEquals('05/14/1988', $contact->birthday);
+        $this->assertEquals('ABC String', $contact->company);
+
+    }
+
+    /** @test */
+    public function a_name_is_required()
+    {
+//        $this->withoutExceptionHandling();
+
+        $response = $this->post('api/contacts', [
+            'email' => 'test@email.com',
+            'birthday' => '05/14/1988',
+            'company' => 'ABC String'
+        ]);
+
+        $response->assertSessionHasErrors('name');
+        $this->assertCount(0, Contact::all());
+    }
+
+    /**
+     * @test
+     */
+
+    public function email_is_required()
+    {
+        $response = $this->post('/api/contacts', [
+            'name' => 'Test Name',
+            'birthday' => '05/14/1988',
+            'company' => 'ABC String'
+        ]);
+
+        $response->assertSessionHasErrors('email');
+        $this->assertCount(0, Contact::all());
     }
 }
